b947157fa1f68116800e46acb10fe13ffb9c7112
Buenno <krijoh.b@gmail.com>
2022-10-04 14:29:40 +0200

    somebody help

--- sess4.hs
+++ sess4.hs
@@ -1,8 +1,9 @@
 -- recursion
+import Data.List(group)
 
 -- opg 1, requires Eq to see if Equal to 1. 
 -- Takes absolut value to handle negative error
-rep :: (Eq n, Num n) => x -> n -> [x]
+rep :: x -> Int -> [x]
 rep x 1 = [x]
 rep x n = x:rep x (abs (n-1))
 
@@ -12,3 +13,13 @@ improve [] = []
 improve (x:xs)
     | odd (length xs) = improve xs
     | otherwise = x:improve xs
+
+rev :: [a] -> [a]
+rev [] = []
+rev [x] = [x]
+rev (x:xs) = (rev xs) ++ [x]
+
+myLast :: [a] -> a
+myLast [x] = x
+myLast (x:xs) = myLast xs
+
